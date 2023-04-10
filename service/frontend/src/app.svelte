63dff6af7ff998ace1a77909603534bdebe66412
Yersa Nordman <yersa@finwo.nl>
2023-04-11 00:08:29 +0200

    Functional navigation after login

--- service/frontend/src/app.svelte
+++ service/frontend/src/app.svelte
@@ -2,29 +2,19 @@
   import { onDestroy } from 'svelte';
   import { Router    } from 'svelte-router-spa';
   import * as apiClient from './lib/api-client';
-
-  import DefaultLayout from './components/layout/default.svelte';
+  import { routes } from './lib/routes';
 
   import Spinner       from './components/loader/spinner.svelte';
-  import HomePage      from './components/page/home.svelte';
   import LoginPage     from './components/page/login.svelte';
-  import NotFoundPage  from './components/page/not-found.svelte';
 
   function authGuard() {
     return apiClient.isLoggedIn.value;
   }
 
-  const routes = [
-    { name: '/'  , component: HomePage     , layout: DefaultLayout },
-    { name: '404', component: NotFoundPage , layout: DefaultLayout },
-  ];
-
-
   let isLoading = true;
   let isLoggedIn = false;
   onDestroy(apiClient.isLoading.subscribe(value => isLoading = value));
   onDestroy(apiClient.isLoggedIn.subscribe(value => isLoggedIn = value));
-
 </script>
 
 {#if isLoading}
--- service/frontend/src/components/layout/default.svelte
+++ service/frontend/src/components/layout/default.svelte
@@ -1,13 +1,26 @@
 <script>
-  import { Route } from 'svelte-router-spa';
+  import { Route, Navigate } from 'svelte-router-spa';
   import { logout } from '../../lib/api-client';
+  import { routes } from '../../lib/routes';
 
   export let currentRoute;
+
+  let filteredRoutes = routes.filter(route => {
+    if (!route.name.startsWith('/')) return false;
+    if (route.name == '/') return false;
+    return true;
+  });
 </script>
 
 <button id=logoutButton on:click={logout}>
   Logout
 </button>
+
+<nav>
+  {#each filteredRoutes as route}
+    <span><Navigate to={route.name}>{route.label}</Navigate></span>
+  {/each}
+</nav>
 
 <Route {currentRoute}/>
 
@@ -16,5 +29,19 @@
     position : absolute;
     top      : 1em;
     right    : 1em;
+  }
+  nav {
+    border-bottom: 1px solid var(--border);
+    margin-bottom: 1em;
+    padding-bottom: 1em;
+  }
+  nav span {
+    display: inline-block;
+    text-transform: uppercase;
+    border-left: 1px solid var(--border);
+    padding: 6px 1em;
+  }
+  nav span:last-child {
+    border-right: 1px solid var(--border);
   }
 </style>
